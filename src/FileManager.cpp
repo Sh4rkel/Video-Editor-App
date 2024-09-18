77f2c07baf3066336b2107c14ad0f9ba48d98d5b
Daniel Filip <filipdaniel2000@yahoo.com>
2024-09-18 19:07:35 +0300

    comments + code organization

--- src/FileManager.cpp
+++ src/FileManager.cpp
@@ -1,11 +1,14 @@
 #include "FileManager.h"
 
+// Constructor
 FileManager::FileManager(QObject *parent) : QObject(parent) {}
 
+// Add video file path to the list
 void FileManager::addVideo(const QString &filePath) {
     videoList.append(filePath);
 }
 
+// Get the list of video file paths
 QStringList FileManager::getVideos() const {
     return videoList;
 }
--- src/SpeedWidget.cpp
+++ src/SpeedWidget.cpp
@@ -1,22 +1,27 @@
 #include "SpeedWidget.h"
 
+// Constructor
 SpeedWidget::SpeedWidget(QWidget *parent) : QWidget(parent) {
     QVBoxLayout *layout = new QVBoxLayout(this);
 
+    // Initialize speed label
     speedLabel = new QLabel("Speed: 1.0x", this);
     speedLabel->setStyleSheet("color: black;");
 
+    // Initialize speed slider
     speedSlider = new QSlider(Qt::Horizontal, this);
     speedSlider->setRange(50, 200);
     speedSlider->setValue(100);
     connect(speedSlider, &QSlider::valueChanged, this, &SpeedWidget::onSpeedSliderChanged);
 
+    // Add widgets to layout
     layout->addWidget(speedLabel);
     layout->addWidget(speedSlider);
 
     setLayout(layout);
 }
 
+// Handle speed slider value change
 void SpeedWidget::onSpeedSliderChanged(int value) {
     double speed = value / 100.0;
     speedLabel->setText(QString("Speed: %1x").arg(speed));
