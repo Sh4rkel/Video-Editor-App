b60ac562459f3387c055805139d28e60f83a56de
Daniel Filip <filipdaniel2000@yahoo.com>
2024-09-18 19:05:22 +0300

    comments + code organization

--- src/VideoFrameWidget.cpp
+++ src/VideoFrameWidget.cpp
@@ -2,13 +2,16 @@
 #include <QPainter>
 #include <QDebug>
 
+// Constructor
 VideoFrameWidget::VideoFrameWidget(QWidget *parent) : QWidget(parent) {}
 
+// Set the current frame to display
 void VideoFrameWidget::setFrame(const QImage &frame) {
     currentFrame = frame;
     update();
 }
 
+// Paint event to render the current frame
 void VideoFrameWidget::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
     if (!currentFrame.isNull()) {
--- src/VideoPlayerWidget.cpp
+++ src/VideoPlayerWidget.cpp
@@ -3,29 +3,35 @@
 #include <QUrl>
 #include <QMediaPlayer>
 
+// Constructor
 VideoPlayerWidget::VideoPlayerWidget(QWidget *parent)
     : QWidget(parent), ui(new Ui::VideoPlayerWidget), mediaPlayer(new QMediaPlayer(this)) {
     ui->setupUi(this);
     mediaPlayer->setVideoOutput(ui->videoWidget);
 }
 
+// Destructor
 VideoPlayerWidget::~VideoPlayerWidget() {
     delete ui;
 }
 
+// Load video file
 void VideoPlayerWidget::loadVideo(const QString &fileName) {
     mediaPlayer->setSource(QUrl::fromLocalFile(fileName));
     mediaPlayer->play();
 }
 
+// Get media player instance
 QMediaPlayer* VideoPlayerWidget::getMediaPlayer() const {
     return mediaPlayer;
 }
 
+// Seek to a specific position in the video
 void VideoPlayerWidget::seek(int position) {
     mediaPlayer->setPosition(position);
 }
 
+// Set playback speed
 void VideoPlayerWidget::setSpeed(double speed) {
     mediaPlayer->setPlaybackRate(speed);
 }
