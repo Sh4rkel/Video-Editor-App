85b7d9780c2a500673260af43f83db4d0b023160
Daniel Filip <filipdaniel2000@yahoo.com>
2024-09-18 19:09:37 +0300

    comments + code organization

--- src/SpeedDialog.cpp
+++ src/SpeedDialog.cpp
@@ -1,8 +1,10 @@
 #include "SpeedDialog.h"
 
+// Constructor
 SpeedDialog::SpeedDialog(QWidget *parent) : QDialog(parent) {
     QVBoxLayout *layout = new QVBoxLayout(this);
 
+    // Initialize speed widget
     speedWidget = new SpeedWidget(this);
     layout->addWidget(speedWidget);
 
