1b00b87d911ad649198318da5bd1175a6856bd29
Daniel Filip <filipdaniel2000@yahoo.com>
2024-09-18 19:00:05 +0300

    comments + code organization

--- src/FilterSettings.cpp
+++ src/FilterSettings.cpp
@@ -1,6 +1,7 @@
 #include "FilterSettings.h"
 #include "ui_FilterSettings.h"
 
+// Constructor
 FilterSettings::FilterSettings(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::FilterSettings),
@@ -9,22 +10,29 @@ FilterSettings::FilterSettings(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    // Connect apply button to accept dialog
     connect(ui->applyButton, &QPushButton::clicked, this, &FilterSettings::accept);
+
+    // Connect cancel button to reject dialog
     connect(ui->cancelButton, &QPushButton::clicked, this, &FilterSettings::reject);
 }
 
+// Destructor
 FilterSettings::~FilterSettings() {
     delete ui;
 }
 
+// Get selected filter from combo box
 QString FilterSettings::getSelectedFilter() const {
     return ui->filterComboBox->currentText();
 }
 
+// Get gradient value
 int FilterSettings::getGradient() const {
     return gradient;
 }
 
+// Set gradient value and emit settingsChanged signal if value changes
 void FilterSettings::setGradient(int value) {
     if (gradient != value) {
         gradient = value;
@@ -32,10 +40,12 @@ void FilterSettings::setGradient(int value) {
     }
 }
 
+// Get shadow value
 int FilterSettings::getShadow() const {
     return shadow;
 }
 
+// Set shadow value and emit settingsChanged signal if value changes
 void FilterSettings::setShadow(int value) {
     if (shadow != value) {
         shadow = value;
