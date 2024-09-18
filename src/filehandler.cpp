5df3183710e943a42d16920f08d6901b2a0de7ed
Daniel Filip <filipdaniel2000@yahoo.com>
2024-09-18 19:09:17 +0300

    comments + code organization

--- src/filehandler.cpp
+++ src/filehandler.cpp
@@ -3,19 +3,24 @@
 #include <QFileDialog>
 #include <QListWidgetItem>
 
+// Constructor
 FileHandler::FileHandler(QWidget *parent) :
     QWidget(parent), ui(new Ui::FileHandler), fileManager(new FileManager(this)) {
     ui->setupUi(this);
 
+    // Connect add video button to slot
     connect(ui->addVideoButton, &QPushButton::clicked, this, &FileHandler::onAddVideoButtonClicked);
 
+    // Connect video list widget item double-clicked signal to slot
     connect(ui->videoListWidget, &QListWidget::itemDoubleClicked, this, &FileHandler::onVideoItemDoubleClicked);
 }
 
+// Destructor
 FileHandler::~FileHandler() {
     delete ui;
 }
 
+// Slot for add video button clicked
 void FileHandler::onAddVideoButtonClicked() {
     QString filePath = QFileDialog::getOpenFileName(this, tr("Open Video"), "", tr("Video Files (*.mp4 *.avi *.mov)"));
     if (!filePath.isEmpty()) {
@@ -24,6 +29,7 @@ void FileHandler::onAddVideoButtonClicked() {
     }
 }
 
+// Slot for video item double-clicked
 void FileHandler::onVideoItemDoubleClicked(QListWidgetItem *item) {
     emit fileSelected(item->text());
 }
